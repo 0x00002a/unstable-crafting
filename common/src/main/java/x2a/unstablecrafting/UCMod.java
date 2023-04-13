1a988887246956f5ccbcf85ff46fb974c4553793
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2023-04-13 23:39:28 +0100

    feat: recipe replacement working

--- common/src/main/java/x2a/unstablecrafting/UCMod.java
+++ common/src/main/java/x2a/unstablecrafting/UCMod.java
@@ -10,15 +10,20 @@ import dev.architectury.registry.registries.RegistrySupplier;
 import net.minecraft.client.Game;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.Registry;
+import net.minecraft.network.protocol.game.ClientboundUpdateRecipesPacket;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Recipe;
 import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.item.crafting.ShapedRecipe;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import x2a.unstablecrafting.mixins.RecipeMixin;
+import net.minecraft.network.protocol.game.ClientboundRecipePacket;
 
 import java.util.*;
 import java.util.function.Supplier;
@@ -29,34 +34,42 @@ public class UCMod {
     public static final Logger Log = LogManager.getLogger("Unstable Crafting");
 
     private static final Random RAND = new Random();
+    public static final Map<ItemStack, ItemStack> RECIPE_REDIRECTS = new HashMap<>();
 
-    static void randomiseRecipes(RecipeManager target) {
+    static void randomiseRecipes(MinecraftServer server) {
+        var target = server.getRecipeManager();
         var outputs = new ArrayList<>(target.getRecipes());
         outputs.sort(Comparator.comparing(Recipe::getId)); // this is so our rng is seed determined
         Collections.shuffle(outputs, RAND);
         var recipes = target.getRecipes();
         var replacement = new ArrayList<Recipe<?>>();
         replacement.ensureCapacity(recipes.size());
+
+        RECIPE_REDIRECTS.clear();
         for (var recipe : recipes) {
             var repl = outputs.remove(outputs.size() - 1).getResultItem();
-            RandomRecipe<?> replaceRecipe;
-            if (recipe instanceof RandomRecipe<?> r) {
+            RECIPE_REDIRECTS.put(recipe.getResultItem(), repl);
+        }
+        server.getPlayerList().broadcastAll(new ClientboundUpdateRecipesPacket(target.getRecipes()));
+        /*for (var recipe : recipes) {
+            var repl = outputs.remove(outputs.size() - 1).getResultItem();
+            var replace = (RecipeMixin)recipe;
+            RecipeMixin<?> replaceRecipe;
+            if (recipe instanceof RecipeMixin<?> r) {
                 replaceRecipe = r;
             } else {
                 replaceRecipe = new RandomRecipe<>(recipe);
             }
             replaceRecipe.newOutput = repl;
             replacement.add(replaceRecipe);
-        }
-        target.replaceRecipes(replacement);
+        }*/
     }
 
     public static void init() {
         Log.info("Welcome to the world of recipes with half lives");
         LifecycleEvent.SERVER_STARTING.register(server -> {
-            var manager = server.getRecipeManager();
             RAND.setSeed(server.getWorldData().worldGenSettings().seed());
-            randomiseRecipes(manager);
+            randomiseRecipes(server);
         });
     }
 }
