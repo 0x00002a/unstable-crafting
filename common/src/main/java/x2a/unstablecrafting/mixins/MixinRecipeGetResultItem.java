b2a2b29a17012e23d64f2e1eb61faaf5ff2a2992
Natasha England-Elbro <ashenglandelbro@protonmail.com>
2023-04-15 21:12:12 +0100

    fix: get result item injection failing

--- common/src/main/java/x2a/unstablecrafting/mixins/MixinRecipeGetResultItem.java
+++ common/src/main/java/x2a/unstablecrafting/mixins/MixinRecipeGetResultItem.java
@@ -1,10 +1,8 @@
 package x2a.unstablecrafting.mixins;
 
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.*;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -13,13 +11,9 @@ import x2a.unstablecrafting.UCMod;
 
 @Mixin({ShapedRecipe.class, AbstractCookingRecipe.class, ShapelessRecipe.class, CustomRecipe.class})
 public abstract class MixinRecipeGetResultItem {
-
-    @Shadow
-    public abstract ResourceLocation getId();
-
     @Inject(at = @At("RETURN"), method = "getResultItem", cancellable = true)
     public void unstablecrafting$afterGetResultItem(CallbackInfoReturnable<ItemStack> cir) {
-        var replace = UCMod.replaceResult(this.getId());
+        var replace = UCMod.replaceResult(((Recipe<?>) this).getId());
         if (replace != null) {
             cir.setReturnValue(replace);
         }
